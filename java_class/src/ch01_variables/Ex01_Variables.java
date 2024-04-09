79cbc833137bf119b27016c334061cbfb8b5d6f1
codingrecipe1 <codingrecipe1@gmail.com>
2024-04-09 20:23:19 +0900

    변수

---
+++ java_class/src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,31 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 10을 정수형 변수 num1에 저장하면서 선언 
+		int num1 = 10;
+		int num2 = 0;
+		// 변수값을 콘솔에 출력 
+		System.out.println(num1);
+		System.out.println("num1");
+		
+		// 1.23456 이라는 값을 num3 변수에 저장 
+		double num3 = 1.23456;
+		System.out.println(num3);
+		// num4 = 1.23456;
+		double num5 = 1;
+		
+		boolean bool1 = true;
+		boolean bool2 = false;
+		// boolean bool3 = 10;
+		
+		String str1 = "안녕하세요.";
+		String str2 = "hello";
+		String str3 = "오늘은 화요일. 오후 7시 53분";
+		System.out.println(str1);
+
+	}
+
+}
---
+++ java_class/src/ch01_variables/Ex02_Variables.java
@@ -0,0 +1,28 @@
+package ch01_variables;
+
+public class Ex02_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 본인의 이름(name), 나이(age), 키(height), 주소(address)
+		// 전화번호(mobile)를 저장하는 변수를 각각 선언하고 
+		// 변수값을 출력 
+		String name = "손흥민";
+		int age = 31;
+		double height = 183.0;
+		String address = "영국 런던";
+		String mobile = "01011111111";
+		String mobile1 = "010";
+		String mobile2 = "1111";
+		String mobile3 = "3333";
+		int mobile4 = 01022222222;
+		System.out.println(mobile4);
+		
+		System.out.println(name);
+		System.out.println("이름: " + name);
+		System.out.println("저는 " + age + "살 입니다.");
+		System.out.println("키는 " + height + "cm 입니다.");
+		System.out.println("저는 " + address + "에 살고 있습니다.");
+	}
+
+}
