ba3eb68307710a2c209e69e9b8b7cc93d6442f16
codingrecipe1 <codingrecipe1@gmail.com>
2024-04-30 17:46:27 +0900

    객체지향 프로그래밍 연습 - 도서관리2

--- java_class/src/ch13_classes/ex02/BookRepository.java
+++ java_class/src/ch13_classes/ex02/BookRepository.java
@@ -6,4 +6,57 @@ import java.util.List;
 public class BookRepository {
     private static List<BookDTO> bookDTOList = new ArrayList<>();
     private static long bookId = 1L;
+
+    public boolean save(BookDTO bookDTO) {
+        bookDTO.setId(bookId++);
+        return bookDTOList.add(bookDTO);
+    }
+
+    public List<BookDTO> findAll() {
+        return bookDTOList;
+    }
+
+    public BookDTO findById(long id) {
+        BookDTO bookDTO = null;
+//        for (int i = 0; i < bookDTOList.size(); i++) {
+//            if (id == bookDTOList.get(i).getId()) {
+//                bookDTO = bookDTOList.get(i);
+//            }
+//        }
+        // for each
+        for (BookDTO bookDTO1: bookDTOList) {
+            if (id == bookDTO1.getId()) {
+                bookDTO = bookDTO1;
+            }
+        }
+
+        return bookDTO;
+    }
+
+    public BookDTO findByTitle(String bookTitle) {
+        BookDTO bookDTO = null;
+        for (int i = 0; i < bookDTOList.size(); i++) {
+            if (bookTitle.equals(bookDTOList.get(i).getBookTitle())) {
+                bookDTO = bookDTOList.get(i);
+            }
+        }
+        return bookDTO;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- java_class/src/ch13_classes/ex02/BookService.java
+++ java_class/src/ch13_classes/ex02/BookService.java
@@ -1,7 +1,73 @@
 package ch13_classes.ex02;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class BookService {
     private BookRepository bookRepository = new BookRepository();
+    Scanner scanner = new Scanner(System.in);
 
+    public void save() {
+        System.out.print("도서명: ");
+        String bookTitle = scanner.next();
+        System.out.print("저자: ");
+        String bookAuthor = scanner.next();
+        System.out.print("가격: ");
+        int bookPrice = scanner.nextInt();
+        System.out.print("출판사: ");
+        String bookPublisher = scanner.next();
+        BookDTO bookDTO = new BookDTO(bookTitle, bookAuthor, bookPrice, bookPublisher);
+        boolean result = bookRepository.save(bookDTO);
+        if (result) {
+            System.out.println("등록 성공");
+        } else {
+            System.out.println("등록 실패");
+        }
+    }
 
+    public void findAll() {
+        List<BookDTO> bookDTOList = bookRepository.findAll();
+        for (BookDTO bookDTO: bookDTOList) {
+            System.out.println("bookDTO = " + bookDTO);
+        }
+    }
+
+    public void findById() {
+        System.out.print("조회 id: ");
+        long id = scanner.nextLong();
+        BookDTO bookDTO = bookRepository.findById(id);
+        if (bookDTO != null) {
+            System.out.println("bookDTO = " + bookDTO);
+        } else {
+            System.out.println("조회결과가 없습니다!");
+        }
+    }
+
+    public void findByTitle() {
+        System.out.print("도서명: ");
+        String bookTitle = scanner.next();
+        BookDTO bookDTO = bookRepository.findByTitle(bookTitle);
+        if (bookDTO != null) {
+            System.out.println("bookDTO = " + bookDTO);
+        } else {
+            System.out.println("조회결과가 없습니다!");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
